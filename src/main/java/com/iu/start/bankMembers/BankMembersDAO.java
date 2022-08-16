dd307c6ae60ba35136927be1c747ebf523c3ce58
seok2609 <hjs120926@naver.com>
2022-08-16 17:46:47 +0900

    0816

--- src/main/java/com/iu/start/bankMembers/BankMembersDAO.java
+++ src/main/java/com/iu/start/bankMembers/BankMembersDAO.java
@@ -5,12 +5,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.iu.start.util.DBConnector;
 
 @Repository
 public class BankMembersDAO implements MembersDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	public final String NAMESPACE = "com.iu.start.bankMembers.BankMembersDAO.";
 	
 	@Override
 	public int setJoin(BankMembersDTO bankMembersDTO) throws Exception {
@@ -44,70 +50,73 @@ public class BankMembersDAO implements MembersDAO {
 	@Override
 	public ArrayList<BankMembersDTO> getSearchByID(String search) throws Exception {
 		ArrayList<BankMembersDTO> ar = new ArrayList<BankMembersDTO>();
-		//1. DB 연결
-		Connection con = DBConnector.getConnection();
+//		//1. DB 연결
+//		Connection con = DBConnector.getConnection();
+//		
+//		//2. Query문 작성
+//		String sql = "SELECT * FROM BANKMEMBERS WHERE USERNAME LIKE ? ORDER BY USERNAME ASC";
+//		
+//		//3. Query문 미리 전송
+//		PreparedStatement st = con.prepareStatement(sql);
+//		
+//		//4. ?값 세팅
+//		st.setString(1, "%"+search+"%");
+//		
+//		
+//		//5. 최종 전송 후 출력
+//		ResultSet rs = st.executeQuery();
+//		
+//		while(rs.next()) {
+//			BankMembersDTO bankMembersDTO = new BankMembersDTO();
+//			bankMembersDTO.setUserName(rs.getString("USERNAME"));
+//			bankMembersDTO.setPassWord(rs.getString("PASSWORD"));
+//			bankMembersDTO.setName(rs.getString("NAME"));
+//			bankMembersDTO.setEmail(rs.getString("EMAIL"));
+//			bankMembersDTO.setPhone(rs.getString("PHONE"));
+//			ar.add(bankMembersDTO);
+//		}
+//		//6. 자원해제
+//		
+//		DBConnector.disConnect(rs, st, con);
 		
-		//2. Query문 작성
-		String sql = "SELECT * FROM BANKMEMBERS WHERE USERNAME LIKE ? ORDER BY USERNAME ASC";
-		
-		//3. Query문 미리 전송
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		//4. ?값 세팅
-		st.setString(1, "%"+search+"%");
-		
-		
-		//5. 최종 전송 후 출력
-		ResultSet rs = st.executeQuery();
-		
-		while(rs.next()) {
-			BankMembersDTO bankMembersDTO = new BankMembersDTO();
-			bankMembersDTO.setUserName(rs.getString("USERNAME"));
-			bankMembersDTO.setPassWord(rs.getString("PASSWORD"));
-			bankMembersDTO.setName(rs.getString("NAME"));
-			bankMembersDTO.setEmail(rs.getString("EMAIL"));
-			bankMembersDTO.setPhone(rs.getString("PHONE"));
-			ar.add(bankMembersDTO);
-		}
-		//6. 자원해제
-		
-		DBConnector.disConnect(rs, st, con);
-		
-		return ar;
+		return sqlSession.selectOne(NAMESPACE+"getSearchByID", search);
 	}
 	
 	public BankMembersDTO getLogin(BankMembersDTO bankMembersDTO) throws Exception {
 		
-		//DB연결
-		Connection con = DBConnector.getConnection();
+//		//DB연결
+//		Connection con = DBConnector.getConnection(); 
+//		
+//		//Query문 작성
+//		String sql = "SELECT USERNAME, NAME FROM BANKMEMBERS WHERE USERNAME=? and PASSWORD =?";
+//		
+//		//Query문 미리전송
+//		PreparedStatement st = con.prepareStatement(sql);
+//		
+//		//?값 있으면 세팅
+//		st.setString(1, bankMembersDTO.getUserName());
+//		st.setString(2, bankMembersDTO.getPassWord());
+//		
+//		//최종 결과 처리후 전송
+//		ResultSet rs = st.executeQuery();
+//		
+//		
+//		if(rs.next()) {
+//			bankMembersDTO = new BankMembersDTO();
+//			bankMembersDTO.setUserName(rs.getString("USERNAME"));
+//			bankMembersDTO.setName(rs.getString("NAME"));
+//		}else {
+//			bankMembersDTO = null;
+//			//return = null;
+//		}
+//		
+//		//자원해제
+//		DBConnector.disConnect(rs, st, con);
+		System.out.println(bankMembersDTO.getUserName());
+		System.out.println(bankMembersDTO.getPassWord());
 		
-		//Query문 작성
-		String sql = "SELECT USERNAME, NAME FROM BANKMEMBERS WHERE USERNAME=? and PASSWORD =?";
 		
-		//Query문 미리전송
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		//?값 있으면 세팅
-		st.setString(1, bankMembersDTO.getUserName());
-		st.setString(2, bankMembersDTO.getPassWord());
-		
-		//최종 결과 처리후 전송
-		ResultSet rs = st.executeQuery();
-		
-		
-		if(rs.next()) {
-			bankMembersDTO = new BankMembersDTO();
-			bankMembersDTO.setUserName(rs.getString("USERNAME"));
-			bankMembersDTO.setName(rs.getString("NAME"));
-		}else {
-			bankMembersDTO = null;
-			//return = null;
-		}
-		
-		//자원해제
-		DBConnector.disConnect(rs, st, con);
-		
-		return bankMembersDTO;
+		return sqlSession.selectOne(NAMESPACE+"getLogin", bankMembersDTO);
 	}
 	
 	
