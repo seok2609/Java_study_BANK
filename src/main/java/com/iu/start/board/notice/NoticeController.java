e5c63947a45c4bb9e267b1a360730038f090934b
seok2609 <hjs120926@naver.com>
2022-09-13 15:43:09 +0900

    배포 전 에러처리

--- src/main/java/com/iu/start/board/notice/NoticeController.java
+++ src/main/java/com/iu/start/board/notice/NoticeController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,6 +49,10 @@ public class NoticeController {
 		mv.addObject("list", ar);
 //		mv.addObject("board", "Notice");
 		mv.setViewName("board/list");
+		
+		if(ar.size() != 0 ) {
+			throw new Exception();
+		}
 		
 		return mv;
 	}
@@ -146,5 +151,20 @@ public class NoticeController {
 		return "redirect:./list.iu";
 	}
 	
+	@ExceptionHandler(NullPointerException.class)
+	public ModelAndView exceptionTest() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("errors/error_404");
+		return mv;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView exceptionTest2(Exception e) {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("errors/error_404");
+		return mv;
+	}
 	
 }
---
+++ src/main/java/com/iu/start/error/FrontErrorController.java
@@ -0,0 +1,30 @@
+package com.iu.start.error;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/error/*")
+public class FrontErrorController {
+	
+	@GetMapping("error400")
+	public ModelAndView errer400() throws Exception{
+		ModelAndView mv = new ModelAndView();
+		
+		System.out.println("Parameter가 틀림");
+		mv.setViewName("errors/error_404");
+		return mv;
+		
+	}
+	@GetMapping("error500")
+	public ModelAndView errer500() throws Exception{
+		ModelAndView mv = new ModelAndView();
+		
+		System.out.println("Server Exception 발생");
+		mv.setViewName("errors/error_404");
+		return mv;
+		
+	}
+}
